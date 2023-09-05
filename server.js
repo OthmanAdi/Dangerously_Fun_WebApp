85c8fe11b8820e9dd17ace62632635a38422fe2c
CodingWithAdi <78882424+OthmanAdi@users.noreply.github.com>
2023-09-05 02:30:36 +0200

    backend server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const { exec } = require('child_process');
+
+const app = express();
+app.use(cors());  // Enable CORS for all routes
+app.use(express.json());
+
+app.post('/api/download', (req, res) => {
+    const { url, format } = req.body;
+
+    // IMPORTANT: Be VERY careful with this; running shell commands
+    // from user input can be risky. Always sanitize and validate the input.
+    const formatCommand = format ? `-f ${format}` : '';
+    exec(`spotdl ${url} ${formatCommand}`, (error, stdout, stderr) => {
+        if (error) {
+            return res.status(500).json({ error: 'Download failed' });
+        }
+        res.status(200).json({ success: true, message: 'Downloaded successfully!' });
+    });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
